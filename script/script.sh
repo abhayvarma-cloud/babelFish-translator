410cbbae2a5157dc92df69c358b51437abefba03
abhay <abhayvarma@gmail.com>
2025-06-12 22:43:31 -0400

    initial coommit

---
+++ script/script.sh
@@ -0,0 +1,3 @@
+python3.11 -m venv my_env
+source my_env/bin/activate # activate my_env
+pip install -r requirements.txt
---
+++ worker.py
@@ -0,0 +1,48 @@
+# To call watsonx's LLM, we need to import the library of IBM Watson Machine Learning
+from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
+from ibm_watson_machine_learning.foundation_models import Model
+
+# placeholder for Watsonx_API and Project_id incase you need to use the code outside this environment
+# API_KEY = "Your WatsonX API"
+PROJECT_ID= "skills-network"
+
+# Define the credentials 
+credentials = {
+    "url": "https://us-south.ml.cloud.ibm.com"
+    #"apikey": API_KEY
+}
+    
+# Specify model_id that will be used for inferencing
+model_id = ModelTypes.FLAN_UL2
+
+# Define the model parameters
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
+
+parameters = {
+    GenParams.DECODING_METHOD: DecodingMethods.GREEDY,
+    GenParams.MIN_NEW_TOKENS: 1,
+    GenParams.MAX_NEW_TOKENS: 1024
+}
+
+# Define the LLM
+model = Model(
+    model_id=model_id,
+    params=parameters,
+    credentials=credentials,
+    project_id=PROJECT_ID
+)
+
+def speech_to_text(audio_binary):
+    return None
+
+def text_to_speech(text, voice=""):
+    return None
+
+def watsonx_process_message(user_message):
+    # Set the prompt for Watsonx API
+    prompt = f"""You are an assistant helping translate sentences from English into Spanish.
+    Translate the query to Spanish: ```{user_message}```."""
+    response_text = model.generate_text(prompt=prompt)
+    print("wastonx response:", response_text)
+    return response_text
